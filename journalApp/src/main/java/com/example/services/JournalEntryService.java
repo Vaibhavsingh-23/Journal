a67462a175b20dfdc968d33a87b88e039e1969e4
vaibhav singh <vaibhavsinghnmp@gmail.com>
2025-05-28 22:39:32 +0530

    Initial commit for Journal App with Spring Boot and MongoDB

---
+++ journalApp/src/main/java/com/example/services/JournalEntryService.java
@@ -0,0 +1,29 @@
+
+
+package com.example.services;
+import java.util.*;
+import com.example.entity.JournalEntry;
+import com.example.repository.JournalEntryRepository;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JournalEntryService {
+    @Autowired
+    private JournalEntryRepository journalEntryRepository;
+
+    public void saveEntry(JournalEntry journalEntry){
+        journalEntryRepository.save(journalEntry);
+
+    }
+    public List<JournalEntry> getAll(){
+        return journalEntryRepository.findAll();
+    }
+    public Optional<JournalEntry> findById(ObjectId id){
+        return journalEntryRepository.findById(id);
+    }
+    public void deleteById(ObjectId id){
+        journalEntryRepository.deleteById(id);
+    }
+}
